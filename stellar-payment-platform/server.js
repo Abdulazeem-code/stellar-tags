c7d50641d536138fe23863a37256c824cb35139a
Abdulazeem-code <olamilekanabdulazeem@gmail.com>
2026-06-13 21:20:08 +0100

    Update server

--- stellar-payment-platform/server.js
+++ stellar-payment-platform/server.js
@@ -6,6 +6,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(cors());
 app.use(express.json());
